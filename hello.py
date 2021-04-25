ea03a11b6fb90d7ef0c31c8ee6720d5f18226063
Tutuw <83175235+Tutuw@users.noreply.github.com>
2021-04-25 23:07:51 +0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+
+Nama = input ( "Nama Lengkap: ")
+Kelas = input ( "Kelas : ")
+Kampus = input ( "Kampus : ")
+
+print("===============================")
+print("Biodataku")
+print("===============================")
+print("Nama Lengkap : ", Nama)
+print("Kelas : ", Kelas)
+print("Kampus: ", Kampus)
